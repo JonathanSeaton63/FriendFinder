8d571c563641585829d78a49ba22475546852a06
Jon Seaton <jonathanseaton29@gmail.com>
2019-02-16 11:59:13 -0700

    things

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,9 @@
-var friendsData = require("../data/friends");
+var newFriend = require("../data/friends");
+var path = require("path");
 
 module.exports = function (app) {
   app.get("/api/freinds", function (req, res) {
-    res.json(friendsData);
+    res.json(newFriend);
   });
   app.post("/api/friends", function (req, res) {
     var newFriendScores = req.body.scores;
@@ -10,10 +11,10 @@ module.exports = function (app) {
     var friendCount = 0;
     var bestMatch = 0;
   
-    for (var i = 0; i < friendsData.length; i++) {
+    for (var i = 0; i < newFriend.length; i++) {
       var scoresDiff = 0;
       for (var j = 0; j < newFriendScores.length; j++) {
-        scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+        scoresDiff += (Math.abs(parseInt(newFriend[i].scores[j]) - parseInt(newFriendScores[j])));
       }
   
       scoresArray.push(scoresDiff);
@@ -25,37 +26,10 @@ module.exports = function (app) {
       }
     }
   
-    var bff = friendsData[bestMatch];
+    var bff = newFriend[bestMatch];
     res.json(bff);
   
-    friendsData.push(req.body);
+    newFriend.push(req.body);
   });
 
 };
-
-app.post("/api/friends", function (req, res) {
-  var newFriendScores = req.body.scores;
-  var scoresArray = [];
-  var friendCount = 0;
-  var bestMatch = 0;
-
-  for (var i = 0; i < friendsData.length; i++) {
-    var scoresDiff = 0;
-    for (var j = 0; j < newFriendScores.length; j++) {
-      scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
-    }
-
-    scoresArray.push(scoresDiff);
-  }
-
-  for (var i = 0; i < scoresArray.length; i++) {
-    if (scoresArray[i] <= scoresArray[bestMatch]) {
-      bestMatch = i;
-    }
-  }
-
-  var bff = friendsData[bestMatch];
-  res.json(bff);
-
-  friendsData.push(req.body);
-});
