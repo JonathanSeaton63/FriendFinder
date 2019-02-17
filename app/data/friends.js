237f40e0640c67b4184c78f28294717e616b8450
Jon Seaton <jonathanseaton29@gmail.com>
2019-02-16 20:48:31 -0700

    adding addtional friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,18 +2,80 @@ var friends = [ {
     name: "Donovan",
     photo:"https://b.fssta.com/uploads/application/nba/headshots/2353.vresize.350.425.medium.71.png",
     scores:[
+        4,
         5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
+        4,
+        3,
+        1,
+        3,
+        4,
         5,
         5,
         5
       ]
-  },
+  }, {
+    name: "Rudy",
+    photo: "https://b.fssta.com/uploads/application/nba/headshots/1876.vresize.350.425.medium.55.png",
+    scores: [
+      2,
+      2,
+      3,
+      4,
+      4,
+      4,
+      5,
+      1,
+      5,
+      4
+    ]
+  }, {
+    name: "Ricky",
+    photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4011.png&w=350&h=254",
+    scores: [
+      5,
+      5,
+      5,
+      1,
+      4,
+      4,
+      3,
+      4,
+      5,
+      5
+    ]
+
+  }, {
+    name: "Joe",
+    photo: "https://b.fssta.com/uploads/application/nba/headshots/1998.vresize.350.425.medium.27.png",
+    scores: [
+      3,
+      4,
+      2,
+      2,
+      1,
+      4,
+      2,
+      1,
+      1,
+      3
+
+    ]
+  }, {
+    name: "Derrick",
+    photo: "https://b.fssta.com/uploads/application/nba/headshots/1648.vresize.350.425.medium.47.png",
+    scores: [
+      4,
+      3,
+      4,
+      4,
+      5,
+      3,
+      3,
+      2,
+      2,
+      4
+    ]
+  }
 ]
 
   module.exports = friends;
