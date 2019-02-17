2bc382734c4da767afdc0af5cd75531b32ef5de3
Jon Seaton <jonathanseaton29@gmail.com>
2019-02-16 19:26:27 -0700

    Adding new photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [ {
     name: "Donovan",
-    photo:"https://cdn.vox-cdn.com/thumbor/qGkyq0goXVA91eOw7jBIXJS_rwk=/0x0:4200x2775/1200x800/filters:focal(2071x518:2743x1190)/cdn.vox-cdn.com/uploads/chorus_image/image/60804087/955496948.jpg.0.jpg",
+    photo:"https://www.gannett-cdn.com/-mm-/e304762a9e75cc645f2ab3b30d22a886722320fb/c=0-146-3941-5400/local/-/media/2018/04/26/USATODAY/USATODAY/636603009092714492-AP-Thunder-Jazz-Basketball-99361695-1-.JPG?width=534&height=712&fit=crop",
     scores:[
         5,
         5,
