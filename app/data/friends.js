fba301b4f0a4691c0d600a2296ce0c3de243b917
Jon Seaton <jonathanseaton29@gmail.com>
2019-02-16 19:33:59 -0700

    new image

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [ {
     name: "Donovan",
-    photo:"https://www.gannett-cdn.com/-mm-/e304762a9e75cc645f2ab3b30d22a886722320fb/c=0-146-3941-5400/local/-/media/2018/04/26/USATODAY/USATODAY/636603009092714492-AP-Thunder-Jazz-Basketball-99361695-1-.JPG?width=534&height=712&fit=crop",
+    photo:"https://b.fssta.com/uploads/application/nba/headshots/2353.vresize.350.425.medium.71.png",
     scores:[
         5,
         5,
